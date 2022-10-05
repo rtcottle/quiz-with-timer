270589ac0683ed6fff1304251073908ede28f420
Ryan C <111948791+GreyJedi17@users.noreply.github.com>
2022-10-04 23:07:37 -0600

    added comments

---
+++ script.js
@@ -0,0 +1,6 @@
+// TODO: Add timer
+// TODO: add event listener for start button
+// TODO: cycle through questions - - can be through event listener or HTML
+// TODO: present quiz results - local storage
+// TODO: show right/wrong answers
+// TODO: timer reduced when answer is wrong.
