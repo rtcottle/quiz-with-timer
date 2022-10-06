5c1cb391f84a0eef674cbdfcf7422b33ae7c28f9
Ryan C <111948791+GreyJedi17@users.noreply.github.com>
2022-10-06 15:36:07 -0600

    added timer and framework of event listener

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-// TODO: Add timer
 var timeEl = document.querySelector(".time");
 
 var mainEl = document.getElementById("main");
 
+var quizEl = document.querySelector("quiz");
+
+//number of seconds to do the quiz.
 var secondsLeft = 75;
 
 function setTime() {
@@ -19,16 +21,30 @@ function setTime() {
 
 function sendMessage() {
   timeEl.textContent = "Time's Up!";
-  var scoresEl = document.createElement(<header>Final Score</header>);
-  scoresEl.createElement(<button>Try Again</button>);
-  scoresEl.createElement(<button>High Scores</button>);
-  scoresEl.createElement(<input></input>);
-  mainEl.appendChild(scoresEl);
+  var scoresEl = document.createElement("header");
+  var button1 = document.createElement("button");
+  var button2 = document.createElement("button");
+  var input = document.createElement("input");
+  mainEl.textContent = "Final Score: " + secondsLeft;
+  button1.textContent = "Clear History";
+  button2.textContent = "View Highscores";
+  quizEl.appendChild(scoresEl);
+  quizEl.appendChild(scoresEl);
+  quizEl.appendChild(button1);
+  quizEl.appendChild(button2);
+  quizEl.appendChild(input);
 }
 
-setTime();
+// setTime();
+
+quizEl.addEventListener("click", function () {
+  if (secondsLeft < 70) {
+    setTime();
+  }
+});
 
 // TODO: add event listener for start button
+
 // TODO: cycle through questions - - can be through event listener or HTML
 // TODO: present quiz results - local storage
 // TODO: show right/wrong answers
