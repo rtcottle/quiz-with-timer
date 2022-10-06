fbc48789d42ea0565eb0bbf84f904cbcc6638b01
Ryan C <111948791+GreyJedi17@users.noreply.github.com>
2022-10-06 14:39:25 -0600

    framework for timer

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
 // TODO: Add timer
+var timeEl = document.querySelector(".time");
+
+var mainEl = document.getElementById("main");
+
+var secondsLeft = 75;
+
+function setTime() {
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    timeEl.textContent = "Current Score: " + secondsLeft;
+
+    if (secondsLeft === 0) {
+      clearInterval(timerInterval);
+      sendMessage();
+    }
+  }, 1000);
+}
+
+function sendMessage() {
+  timeEl.textContent = "Time's Up!";
+  var scoresEl = document.createElement(<header>Final Score</header>);
+  scoresEl.createElement(<button>Try Again</button>);
+  scoresEl.createElement(<button>High Scores</button>);
+  scoresEl.createElement(<input></input>);
+  mainEl.appendChild(scoresEl);
+}
+
+setTime();
+
 // TODO: add event listener for start button
 // TODO: cycle through questions - - can be through event listener or HTML
 // TODO: present quiz results - local storage
